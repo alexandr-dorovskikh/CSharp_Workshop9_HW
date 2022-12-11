fead0ed79b6985202e35ef87d74a32edb83cae73
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-12-11 11:32:24 +0700

    Task 64 added

---
+++ Task 64/Program.cs
@@ -0,0 +1,26 @@
+﻿void WriteNum(int Num)
+{
+    Console.Write(Num);
+
+    if (Num > 1) Console.Write(", ");
+    
+    int NextNum = Num - 1;
+
+    if (NextNum >= 1) WriteNum(NextNum);
+        
+}
+
+Console.Write("Enter natural number:");
+int FirstNum;
+if (Int32.TryParse(Console.ReadLine(), out FirstNum)) 
+{
+    if (FirstNum <= 0) 
+        Console.WriteLine("You entered not natural number");    
+    else    
+        WriteNum(FirstNum);
+} 
+else
+    Console.WriteLine("You entered wrong value");
+
+
+
